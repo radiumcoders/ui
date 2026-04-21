f4752117808d1a93959ec3d2e81e96da11c1358d
Radiumcoders <radiumcoders@gmail.com>
2026-04-21 18:33:31 +0530

    Update playground.tsx

--- src/routes/playground.tsx
+++ src/routes/playground.tsx
@@ -9,17 +9,7 @@ export const Route = createFileRoute("/playground")({
 function RouteComponent() {
   return (
     <div className="flex h-[calc(100vh-64px)] w-full items-center justify-center">
-      <Stamp stampColor="#DAD8AB">
-        <div className="flex h-full flex-col items-center justify-center p-6 text-center">
-          <div className="mb-2 text-4xl font-bold text-black">STAMP</div>
-          <div className="text-sm text-black">
-            Perforated edge component
-          </div>
-          <div className="mt-4 text-xs text-black/70">
-            Customizable
-          </div>
-        </div>
-      </Stamp>
+      <Stamp />
     </div>
   )
 }
